1e52d0887338bc02929b72fbae9c8e4263523847
Andrey Lebedev <andrey@lebedev.lt>
2012-04-19 11:49:27 +0000

    Back to development

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(*rnames):
 
 setup (
     name='mongopersist',
-    version='0.7.2',
+    version='0.7.3.dev0',
     author = "Stephan Richter",
     author_email = "stephan.richter@gmail.com",
     description = "Mongo Persistence Backend",
