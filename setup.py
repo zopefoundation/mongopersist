0090f3aa872fdae913e46e374e71d1208eac971b
Adam Groszer <agroszer@gmail.com>
2013-06-13 14:19:50 +0200

    Preparing release 0.8.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*rnames):
 
 setup(
     name='mongopersist',
-    version='0.8.4.dev0',
+    version='0.8.4',
     author="Stephan Richter",
     author_email="stephan.richter@gmail.com",
     url='https://github.com/zopefoundation/mongopersist',
