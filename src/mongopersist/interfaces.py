68f71544ede5a22ecb19369d01272f3852841753
Adam Groszer <agroszer@gmail.com>
2013-06-27 16:03:16 +0200

    first stab at getting rid of zope.event

--- src/mongopersist/interfaces.py
+++ src/mongopersist/interfaces.py
@@ -263,3 +263,103 @@ class IMongoSpecProcessor(zope.interface.Interface):
 
     def process(collection, spec):
         """return the processed spec here"""
+
+
+class IMongoContainer(zope.interface.Interface):
+    _m_database = zope.schema.ASCIILine(
+        title=u'Mongo Database',
+        description=(
+            u'Specifies the Mongo DB in which to store items. If ``None``, the '
+            u'default database will be used.'),
+        default=None)
+
+    _m_collection = zope.schema.ASCIILine(
+        title=u'Mongo Collection',
+        description=(
+            u'Specifies the Mongo collection in which to store items.')
+        )
+
+    _m_mapping_key = zope.schema.ASCIILine(
+        title=u'Mapping Key',
+        description=(
+            u'Specifies the attribute name of the item that is used as the '
+            u'mapping/dictionary/container key.'),
+        default='key')
+
+    _m_parent_key = zope.schema.ASCIILine(
+        title=u'Parent Key',
+        description=(
+            u'Specifies the attribute name of the item that is used to store '
+            u'the parent/container reference.'),
+        default='parent')
+
+    _m_remove_documents = zope.schema.Bool(
+        title=u'Remove Documents',
+        description=(
+            u'A flag when set causes documents to be removed from the DB when '
+            u'they are removed from the container.'),
+        default=True)
+
+    def _m_get_parent_key_value():
+        """Returns the value that is used to specify a particular container as
+        the parent of the item.
+        """
+
+    def _m_get_items_filter():
+        """Returns a query spec representing a filter that only returns
+        objects in this container."""
+
+    def _m_add_items_filter(filter):
+        """Applies the item filter items to the provided filter.
+
+        Keys that are already in the passed in filter are not overwritten.
+        """
+
+    def get_collection():
+        """Get the Python representation of the collection.
+
+        This can be useful to make custom queries against the collection.
+        """
+
+    def raw_find(spec=None, *args, **kwargs):
+        """Return a raw Mongo result set for the specified query.
+
+        The spec is updated to also contain the container's filter spec.
+
+        See pymongo's documentation for details on *args and **kwargs.
+        """
+
+    def find(spec=None, fields=None, *args, **kwargs):
+        """Return a Python object result set for the specified query.
+
+        By default only the Mongo Id and key attribute is requested and a
+        ghost is created. The rest of the data is only retrieved if needed.
+
+        The spec is updated to also contain the container's filter spec.
+
+        See pymongo's documentation for details on *args and **kwargs.
+        """
+
+    def raw_find_one(spec_or_id=None, *args, **kwargs):
+        """Return a raw Mongo document for the specified query.
+
+        The spec is updated to also contain the container's filter spec.
+
+        See pymongo's documentation for details on *args and **kwargs.
+        """
+
+    def find_one(spec_or_id=None, fields=None, *args, **kwargs):
+        """Return a single Python object for the specified query.
+
+        The spec is updated to also contain the container's filter spec.
+
+        See pymongo's documentation for details on *args and **kwargs.
+        """
+
+    def add(value, key=None):
+        """Add an object without necessarily knowing the key of the object.
+
+        Either pass in a valid key or the key will be:
+        - in case _m_mapping_key is None: the object's OID
+        - otherwise getattr(value, _m_mapping_key)
+        """
--- src/mongopersist/zope/container.py
+++ src/mongopersist/zope/container.py
@@ -22,49 +22,13 @@ from rwproperty import getproperty, setproperty
 from zope.container import contained, sample
 from zope.container.interfaces import IContainer
 
+import mongopersist.container
 from mongopersist import interfaces
 from mongopersist.zope import interfaces as zinterfaces
 
 
-class MongoContained(contained.Contained):
-
-    _v_name = None
-    _m_name_attr = None
-    _m_name_getter = None
-    _m_name_setter = None
-
-    _m_parent_attr = None
-    _m_parent_getter = None
-    _m_parent_setter = None
-    _v_parent = None
-
-    @getproperty
-    def __name__(self):
-        if self._v_name is None:
-            if self._m_name_attr is not None:
-                self._v_name = getattr(self, self._m_name_attr, None)
-            elif self._m_name_getter is not None:
-                self._v_name = self._m_name_getter()
-        return self._v_name
-    @setproperty
-    def __name__(self, value):
-        if self._m_name_setter is not None:
-            self._m_name_setter(value)
-        self._v_name = value
-
-    @getproperty
-    def __parent__(self):
-        if self._v_parent is None:
-            if self._m_parent_attr is not None:
-                self._v_parent = getattr(self, self._m_parent_attr, None)
-            elif self._m_parent_getter is not None:
-                self._v_parent = self._m_parent_getter()
-        return self._v_parent
-    @setproperty
-    def __parent__(self, value):
-        if self._m_parent_setter is not None:
-            self._m_parent_setter(value)
-        self._v_parent = value
+class MongoContained(contained.Contained, mongopersist.container.MongoContained):
+    pass  # just mix in the zope Contained magic
 
 
 class SimpleMongoContainer(sample.SampleContainer, persistent.Persistent):
@@ -121,203 +85,16 @@ class SimpleMongoContainer(sample.SampleContainer, persistent.Persistent):
 
 
 class MongoContainer(contained.Contained,
-                     persistent.Persistent,
-                     UserDict.DictMixin):
-    zope.interface.implements(IContainer, zinterfaces.IMongoContainer)
-    _m_database = None
-    _m_collection = None
-    _m_mapping_key = 'key'
-    _m_parent_key = 'parent'
-    _m_remove_documents = True
+                     mongopersist.container.MongoContainer):
+    zope.interface.implements(IContainer)
 
-    def __init__(self, collection=None, database=None,
-                 mapping_key=None, parent_key=None):
-        if collection:
-            self._m_collection = collection
-        if database:
-            self._m_database = database
-        if mapping_key is not None:
-            self._m_mapping_key = mapping_key
-        if parent_key is not None:
-            self._m_parent_key = parent_key
-
-    @property
-    def _m_jar(self):
-        if not hasattr(self, '_v_mdmp'):
-            # If the container is in a Mongo storage hierarchy, then getting
-            # the datamanager is easy, otherwise we do an adapter lookup.
-            if interfaces.IMongoDataManager.providedBy(self._p_jar):
-                return self._p_jar
-
-            # cache result of expensive component lookup
-            self._v_mdmp = zope.component.getUtility(
-                    interfaces.IMongoDataManagerProvider)
-
-        return self._v_mdmp.get()
-
-    def get_collection(self):
-        db_name = self._m_database or self._m_jar.default_database
-        return self._m_jar.get_collection(db_name, self._m_collection)
-
-    def _m_get_parent_key_value(self):
-        if getattr(self, '_p_jar', None) is None:
-            raise ValueError('_p_jar not found.')
-        if interfaces.IMongoDataManager.providedBy(self._p_jar):
-            return self
-        else:
-            return 'zodb-'+''.join("%02x" % ord(x) for x in self._p_oid).strip()
-
-    def _m_get_items_filter(self):
-        filter = {}
-        # Make sure that we only look through objects that have the mapping
-        # key. Objects not having the mapping key cannot be part of the
-        # collection.
-        if self._m_mapping_key is not None:
-            filter[self._m_mapping_key] = {'$exists': True}
-        if self._m_parent_key is not None:
-            gs = self._m_jar._writer.get_state
-            filter[self._m_parent_key] = gs(self._m_get_parent_key_value())
-        return filter
-
-    def _m_add_items_filter(self, filter):
-        for key, value in self._m_get_items_filter().items():
-            if key not in filter:
-                filter[key] = value
-
-    def _locate(self, obj, doc):
-        # Helper method that is only used when locating items that are already
-        # in the container and are simply loaded from Mongo.
-        if obj.__name__ is None:
-            obj._v_name = doc[self._m_mapping_key]
-        if obj.__parent__ is None:
-            obj._v_parent = self
-
-    def _load_one(self, doc):
-        # Create a DBRef object and then load the full state of the object.
-        dbref = bson.dbref.DBRef(
-            self._m_collection, doc['_id'],
-            self._m_database or self._m_jar.default_database)
-        # Stick the doc into the _latest_states:
-        self._m_jar._latest_states[dbref] = doc
-        obj = self._m_jar.load(dbref)
-        self._locate(obj, doc)
-        return obj
-
-    def __cmp__(self, other):
-        # UserDict implements the semantics of implementing comparison of
-        # items to determine equality, which is not what we want for a
-        # container, so we revert back to the default object comparison.
-        return cmp(id(self), id(other))
-
-    def __getitem__(self, key):
-        filter = self._m_get_items_filter()
-        filter[self._m_mapping_key] = key
-        obj = self.find_one(filter)
-        if obj is None:
-            raise KeyError(key)
-        return obj
-
-    def _real_setitem(self, key, value):
-        # Make sure the value is in the database, since we might want
-        # to use its oid.
-        if value._p_oid is None:
-            self._m_jar.insert(value)
-
-        # This call by itself causes the state to change _p_changed to True.
-        if self._m_mapping_key is not None:
-            setattr(value, self._m_mapping_key, key)
-        if self._m_parent_key is not None:
-            setattr(value, self._m_parent_key, self._m_get_parent_key_value())
-
-    def __setitem__(self, key, value):
-        # When the key is None, we need to determine it.
-        if key is None:
-            if self._m_mapping_key is None:
-                # Make sure the value is in the database, since we might want
-                # to use its oid.
-                if value._p_oid is None:
-                    self._m_jar.insert(value)
-                key = unicode(value._p_oid.id)
-            else:
-                # we have _m_mapping_key, use that attribute
-                key = getattr(value, self._m_mapping_key)
+    def _after_setitem_hook(self, key, value):
         # We want to be as close as possible to using the Zope semantics.
         contained.setitem(self, self._real_setitem, key, value)
 
-    def add(self, value, key=None):
-        # We are already supporting ``None`` valued keys, which prompts the key
-        # to be determined here. But people felt that a more explicit
-        # interface would be better in this case.
-        self[key] = value
-
-    def __delitem__(self, key):
-        value = self[key]
-        # First remove the parent and name from the object.
-        if self._m_mapping_key is not None:
-            try:
-                delattr(value, self._m_mapping_key)
-            except AttributeError:
-                # Sometimes we do not control those attributes.
-                pass
-        if self._m_parent_key is not None:
-            try:
-                delattr(value, self._m_parent_key)
-            except AttributeError:
-                # Sometimes we do not control those attributes.
-                pass
-        # Let's now remove the object from the database.
-        if self._m_remove_documents:
-            self._m_jar.remove(value)
+    def _after_delitem_hook(self, key, value):
         # Send the uncontained event.
         contained.uncontained(value, self, key)
-
-    def __contains__(self, key):
-        return self.raw_find_one(
-            {self._m_mapping_key: key}, fields=()) is not None
-
-    def __iter__(self):
-        result = self.raw_find(
-            {self._m_mapping_key: {'$ne': None}}, fields=(self._m_mapping_key,))
-        for doc in result:
-            yield doc[self._m_mapping_key]
-
-    def keys(self):
-        return list(self.__iter__())
-
-    def iteritems(self):
-        result = self.raw_find()
-        for doc in result:
-            obj = self._load_one(doc)
-            yield doc[self._m_mapping_key], obj
-
-    def raw_find(self, spec=None, *args, **kwargs):
-        if spec is None:
-            spec = {}
-        self._m_add_items_filter(spec)
-        coll = self.get_collection()
-        return coll.find(spec, *args, **kwargs)
-
-    def find(self, spec=None, *args, **kwargs):
-        # Search for matching objects.
-        result = self.raw_find(spec, *args, **kwargs)
-        for doc in result:
-            obj = self._load_one(doc)
-            yield obj
-
-    def raw_find_one(self, spec_or_id=None, *args, **kwargs):
-        if spec_or_id is None:
-            spec_or_id = {}
-        if not isinstance(spec_or_id, dict):
-            spec_or_id = {'_id': spec_or_id}
-        self._m_add_items_filter(spec_or_id)
-        coll = self.get_collection()
-        return coll.find_one(spec_or_id, *args, **kwargs)
-
-    def find_one(self, spec_or_id=None, *args, **kwargs):
-        doc = self.raw_find_one(spec_or_id, *args, **kwargs)
-        if doc is None:
-            return None
-        return self._load_one(doc)
 
 
 class IdNamesMongoContainer(MongoContainer):
